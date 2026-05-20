3711b7d55415908657b6958d37b0c0df75e52358
Dharmendra Kumar <dharmendra9847@gmail.com>
2026-05-21 02:10:59 +0530

    feat: configure TinyLlama with Spring AI and implement SimpleVectorStore

--- Spring-AI-Project/src/main/java/com/itstech/springai/controller/OpenAIController.java
+++ Spring-AI-Project/src/main/java/com/itstech/springai/controller/OpenAIController.java
@@ -1,19 +1,27 @@
 package com.itstech.springai.controller;
 
+import org.springframework.ai.document.Document;
+import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class OpenAIController {
 
+    private final EmbeddingModel embeddingModel;
     private OllamaChatModel chatModel;
+    private VectorStore vectorStore;
 
     @Autowired
-    public OpenAIController(OllamaChatModel chatModel) {
+    public OpenAIController(OllamaChatModel chatModel, EmbeddingModel embeddingModel, VectorStore vectorStore) {
         this.chatModel = chatModel;
+        this.embeddingModel = embeddingModel;
+        this.vectorStore = vectorStore;
     }
 
     @GetMapping("/api/{message}")
@@ -21,5 +29,30 @@ public class OpenAIController {
 
         String response = chatModel.call(message);
         return response;
+    }
+
+    @PostMapping("/api/similarity")
+    public double getSimilarity(@RequestParam String text1, @RequestParam String text2) {
+
+        float[] embed1 = embeddingModel.embed(text1);
+        float[] embed2 = embeddingModel.embed(text2);
+
+        // FIND SIMILARITY FORMULA using Embedding Model
+        double dotProduct = 0;
+        double norm1 = 0;
+        double norm2 = 0;
+
+        for (int i = 0; i < embed1.length; i++) {
+            dotProduct += embed1[i] * embed2[i];
+            norm1 += Math.pow(embed1[i], 2);
+            norm2 += Math.pow(embed2[i], 2);
+        }
+        return dotProduct / Math.sqrt(norm1) * Math.sqrt(norm2);
+    }
+
+    @PostMapping("/api/product")
+    public List<Document> getProduct(@RequestParam String text) {
+        //return vectorStore.similaritySearch(text);
+        return vectorStore.similaritySearch(SearchRequest.builder().query(text).topK(2).build());
     }
 }
---
+++ Spring-AI-Project/src/main/java/com/itstech/springai/model/DataInitializer.java
@@ -0,0 +1,50 @@
+package com.itstech.springai.model;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.TextReader;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class DataInitializer {
+
+    private VectorStore vectorStore;
+
+    @Autowired
+    public DataInitializer(VectorStore vectorStore) {
+        this.vectorStore = vectorStore;
+    }
+
+    @PostConstruct
+    public void initData() {
+        TextReader textReader =
+                new TextReader(
+                    new ClassPathResource("product_details.txt"));
+
+        // READ DOCUMENTS
+        List<Document> documents = textReader.get();
+
+        // SPLIT DOCUMENTS
+        TokenTextSplitter splitter = TokenTextSplitter
+                .builder()
+                .withChunkSize(100)
+                .withMinChunkSizeChars(30)
+                .withMinChunkLengthToEmbed(5)
+                .withMaxNumChunks(500)
+                .withKeepSeparator(false)
+                .build();
+
+        List<Document> splitDocuments = splitter.apply(documents);
+
+        // STORE IN VECTOR DB
+        vectorStore.add(splitDocuments);
+
+        System.out.println("Documents Loaded Into Vector Store");
+    }
+}
